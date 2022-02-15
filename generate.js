29377b8d21f2e09bbb8037fc9e77d99b177b821e
saurabhdaware <saurabhdaware99@gmail.com>
2022-02-15 20:53:37 +0530

    feat: generate with vite ssr API

--- generate.js
+++ generate.js
@@ -1,20 +1,29 @@
 const fs = require('fs');
 const path = require('path');
+const { createServer } = require('vite');
 
 const INDEX_HTML_PATH = path.resolve(__dirname, 'dist/static/index.html');
-const { render } = require('./dist/server/entry-server.js');
+const ENTRY_SERVER_PATH = `${true ? './dist/server' : '/src'}/entry-server.js`;
 
 
 (async () => {
+  const vite = await createServer({
+    server: { middlewareMode: 'ssr' }
+  })
+  const url = '/';
   // 1. Read
-  const template = fs.readFileSync(INDEX_HTML_PATH, 'utf-8');
+  let template = fs.readFileSync(INDEX_HTML_PATH, 'utf-8');
 
   // 2. Render
-  const appHtml = await render('/');
-  const html = template.replace('<!--server-outlet-->', appHtml);
-
+  template = await vite.transformIndexHtml(url, template)
+  const { render } = require(ENTRY_SERVER_PATH);
+  const appHtml = await render(url)
+  
+  // 5. Inject the app-rendered HTML into the template.
+  const html = template.replace(`<!--server-outlet-->`, appHtml)
   // 3. Write
   fs.writeFileSync(INDEX_HTML_PATH, html);
   console.log('Generated index.html');
   console.log('`npx serve dist/static` to run static server');
+  vite.close();
 })();
